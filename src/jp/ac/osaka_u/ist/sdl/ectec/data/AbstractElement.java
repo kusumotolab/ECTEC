90b3e6d893f52a8c41c2ca75309c921485278a60
k-hotta <k-hotta@ist.osaka-u.ac.jp>
2013-09-17 16:53:51 +0900

    create an abstract class which is the base class of all the elements to be registered into the db

---
+++ src/jp/ac/osaka_u/ist/sdl/ectec/data/AbstractElement.java
@@ -0,0 +1,24 @@
+package jp.ac.osaka_u.ist.sdl.ectec.data;
+
+/**
+ * A class to represent any elements to be registered into the db
+ * 
+ * @author k-hotta
+ * 
+ */
+public abstract class AbstractElement {
+
+	/**
+	 * ID
+	 */
+	protected final long id;
+
+	public AbstractElement(final long id) {
+		this.id = id;
+	}
+
+	public final long getId() {
+		return this.id;
+	}
+
+}
