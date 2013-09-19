0b1ef66a312ce8eb1b7ec03cd893f260fbc08089
k-hotta <k-hotta@ist.osaka-u.ac.jp>
2013-09-19 17:55:10 +0900

    create a class for registering links of clone sets

---
+++ src/jp/ac/osaka_u/ist/sdl/ectec/data/registerer/CloneSetLinkRegisterer.java
@@ -0,0 +1,14 @@
+package jp.ac.osaka_u.ist.sdl.ectec.data.registerer;
+
+import jp.ac.osaka_u.ist.sdl.ectec.data.CloneSetLinkInfo;
+
+/**
+ * A class that represents a registerer for links of clone sets
+ * 
+ * @author k-hotta
+ * 
+ */
+public class CloneSetLinkRegisterer extends
+		AbstractElementRegisterer<CloneSetLinkInfo> {
+
+}
