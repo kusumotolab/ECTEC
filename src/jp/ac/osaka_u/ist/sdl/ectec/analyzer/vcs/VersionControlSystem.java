ad9a64cf1361efa166e759c094c712f5f5499908
k-hotta <k-hotta@ist.osaka-u.ac.jp>
2013-09-19 18:18:37 +0900

    create an enum for representing version conrtol systems

---
+++ src/jp/ac/osaka_u/ist/sdl/ectec/analyzer/vcs/VersionControlSystem.java
@@ -0,0 +1,13 @@
+package jp.ac.osaka_u.ist.sdl.ectec.analyzer.vcs;
+
+/**
+ * An enum that represents version control systems
+ * 
+ * @author k-hotta
+ * 
+ */
+public enum VersionControlSystem {
+
+	SVN, OTHER;
+
+}
