4b7a1a591ba54c292875fc3575af5af921803219
k-hotta <k-hotta@ist.osaka-u.ac.jp>
2013-09-19 18:32:39 +0900

    add a function to get the corresponding version control system from a string

--- src/jp/ac/osaka_u/ist/sdl/ectec/analyzer/vcs/VersionControlSystem.java
+++ src/jp/ac/osaka_u/ist/sdl/ectec/analyzer/vcs/VersionControlSystem.java
@@ -8,6 +8,25 @@ package jp.ac.osaka_u.ist.sdl.ectec.analyzer.vcs;
  */
 public enum VersionControlSystem {
 
-	SVN, OTHER;
+	SVN("svn"), OTHER("n/a");
+
+	private final String str;
+
+	private VersionControlSystem(final String str) {
+		this.str = str;
+	}
+
+	public static final VersionControlSystem getCorrespondingVersionControlSystem(
+			final String str) {
+		if (str.equalsIgnoreCase(SVN.getStr())) {
+			return SVN;
+		} else {
+			return OTHER;
+		}
+	}
+
+	public final String getStr() {
+		return str;
+	}
 
 }
