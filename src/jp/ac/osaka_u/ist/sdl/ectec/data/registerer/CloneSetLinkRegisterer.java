20a1c28c453208b1623b2c36ac6ad197003a4170
k-hotta <k-hotta@ist.osaka-u.ac.jp>
2013-09-19 17:55:19 +0900

    create a class for registering links of clone sets

--- src/jp/ac/osaka_u/ist/sdl/ectec/data/registerer/CloneSetLinkRegisterer.java
+++ src/jp/ac/osaka_u/ist/sdl/ectec/data/registerer/CloneSetLinkRegisterer.java
@@ -1,6 +1,11 @@
 package jp.ac.osaka_u.ist.sdl.ectec.data.registerer;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 import jp.ac.osaka_u.ist.sdl.ectec.data.CloneSetLinkInfo;
+import jp.ac.osaka_u.ist.sdl.ectec.db.DBConnectionManager;
+import jp.ac.osaka_u.ist.sdl.ectec.util.StringUtils;
 
 /**
  * A class that represents a registerer for links of clone sets
@@ -11,4 +16,36 @@ import jp.ac.osaka_u.ist.sdl.ectec.data.CloneSetLinkInfo;
 public class CloneSetLinkRegisterer extends
 		AbstractElementRegisterer<CloneSetLinkInfo> {
 
+	/**
+	 * the constructor
+	 * 
+	 * @param dbManager
+	 * @param maxBatchCount
+	 */
+	public CloneSetLinkRegisterer(DBConnectionManager dbManager,
+			int maxBatchCount) {
+		super(dbManager, maxBatchCount);
+	}
+
+	@Override
+	protected String createPreparedStatementQueue() {
+		return "insert into CLONE_SET_LINK values(?,?,?,?,?,?,?,?,?,?)";
+	}
+
+	@Override
+	protected void setAttributes(PreparedStatement pstmt,
+			CloneSetLinkInfo element) throws SQLException {
+		int column = 0;
+		pstmt.setLong(++column, element.getId());
+		pstmt.setLong(++column, element.getBeforeElementId());
+		pstmt.setLong(++column, element.getAfterElementId());
+		pstmt.setLong(++column, element.getBeforeRevisionId());
+		pstmt.setLong(++column, element.getAfterRevisionId());
+		pstmt.setInt(++column, element.getNumberOfChangedElements());
+		pstmt.setInt(++column, element.getNumberOfAddedElements());
+		pstmt.setInt(++column, element.getNumberOfDeletedElements());
+		pstmt.setInt(++column, element.getNumberOfCoChangedElements());
+		pstmt.setString(++column,
+				StringUtils.convertListToString(element.getCodeFragmentLinks()));
+	}
 }
