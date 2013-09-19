353e0f4d419f0674d96bba9b1acc7bb4457659af
k-hotta <k-hotta@ist.osaka-u.ac.jp>
2013-09-19 18:30:28 +0900

    create an interface for detectors of target revisions

---
+++ src/jp/ac/osaka_u/ist/sdl/ectec/analyzer/vcs/TargetRevisionDetector.java
@@ -0,0 +1,20 @@
+package jp.ac.osaka_u.ist.sdl.ectec.analyzer.vcs;
+
+import java.util.List;
+
+import jp.ac.osaka_u.ist.sdl.ectec.data.RevisionInfo;
+import jp.ac.osaka_u.ist.sdl.ectec.settings.Language;
+
+/**
+ * An interface that represents a detector of target revisions
+ * 
+ * @author k-hotta
+ * 
+ */
+public interface TargetRevisionDetector {
+
+	public List<RevisionInfo> detectTargetRevisions(final Language language,
+			final String startRevisionIdentifier,
+			final String endRevisionIdentifier) throws Exception;
+
+}
