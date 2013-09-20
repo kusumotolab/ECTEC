a0e78a3951a9d98300f344ed7a806bcc769b21c3
k-hotta <k-hotta@ist.osaka-u.ac.jp>
2013-09-20 16:20:35 +0900

    create an interface to have keys of properties files

---
+++ src/jp/ac/osaka_u/ist/sdl/ectec/PropertiesKeys.java
@@ -0,0 +1,27 @@
+package jp.ac.osaka_u.ist.sdl.ectec;
+
+/**
+ * An interface that defines keys of properties files
+ * 
+ * @author k-hotta
+ * 
+ */
+public interface PropertiesKeys {
+
+	public static final String ADDITIONAL_PATH = "ectec.additional-path";
+
+	public static final String LANGUAGE = "ectec.language";
+
+	public static final String THREADS = "ectec.threads";
+
+	public static final String USER_NAME = "ectec.username";
+
+	public static final String PASSWD = "ectec.passwd";
+
+	public static final String START_REVISION_IDENTIFIER = "ectec.start-revision-identifier";
+
+	public static final String END_REVISION_IDENTIFIER = "ectec.end-revision-identifier";
+
+	public static final String VERSION_CONTROL_SYSTEM = "ectec.version-control-system";
+
+}
