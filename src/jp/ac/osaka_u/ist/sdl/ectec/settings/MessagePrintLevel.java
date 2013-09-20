393421c22f8c0148b64372cbe81c08ce6294137a
k-hotta <k-hotta@ist.osaka-u.ac.jp>
2013-09-20 16:20:14 +0900

    [refactoring] reformatting

--- src/jp/ac/osaka_u/ist/sdl/ectec/settings/MessagePrintLevel.java
+++ src/jp/ac/osaka_u/ist/sdl/ectec/settings/MessagePrintLevel.java
@@ -1,5 +1,18 @@
 package jp.ac.osaka_u.ist.sdl.ectec.settings;
 
+/**
+ * An enum that represents the level of verbose output of the current state of
+ * the processing
+ * 
+ * @author k-hotta
+ * 
+ */
 public enum MessagePrintLevel {
+
+	VERBOSE, // the most strong (verbose) output
+	
+	LITTLE, // print only important information
+	
+	NONE; // print nothing
 
 }
