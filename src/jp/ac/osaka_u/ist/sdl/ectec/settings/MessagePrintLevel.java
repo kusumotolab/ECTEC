e9d6f0efd38ba0d871c80ad72170c0594d18990a
k-hotta <k-hotta@ist.osaka-u.ac.jp>
2013-09-20 17:34:34 +0900

    add a function to get a corresponding level from a string

--- src/jp/ac/osaka_u/ist/sdl/ectec/settings/MessagePrintLevel.java
+++ src/jp/ac/osaka_u/ist/sdl/ectec/settings/MessagePrintLevel.java
@@ -9,10 +9,66 @@ package jp.ac.osaka_u.ist.sdl.ectec.settings;
  */
 public enum MessagePrintLevel {
 
-	VERBOSE, // the most strong (verbose) output
-	
-	LITTLE, // print only important information
-	
-	NONE; // print nothing
+	// the most strong (verbose) output
+	VERBOSE(new String[] { "strong", "s", "verbose", "v", "all", "a" }),
+
+	// print only important information
+	LITTLE(new String[] { "little", "l", "weak", "w", "default", "d", "yes",
+			"y" }),
+
+	// print nothing
+	NONE(new String[] { "nothing", "none", "no", "n" });
+
+	/**
+	 * strings that represent the level
+	 */
+	private final String[] correspondingStrs;
+
+	private MessagePrintLevel(final String[] correspondingStrs) {
+		this.correspondingStrs = correspondingStrs;
+	}
+
+	/**
+	 * get the corresponding strings
+	 * 
+	 * @return
+	 */
+	public final String[] getCorrespondingStrs() {
+		return this.correspondingStrs;
+	}
+
+	/**
+	 * judge whether the given string meets any of corresponding strings
+	 * 
+	 * @param str
+	 * @return
+	 */
+	public final boolean corresponds(final String str) {
+		for (final String tmp : correspondingStrs) {
+			if (str.equalsIgnoreCase(tmp)) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	/**
+	 * get the corresponding level with the specified string
+	 * 
+	 * @param str
+	 * @return
+	 */
+	public static MessagePrintLevel getCorrespondingLevel(final String str) {
+		if (VERBOSE.corresponds(str)) {
+			return VERBOSE;
+		} else if (LITTLE.corresponds(str)) {
+			return LITTLE;
+		} else if (NONE.corresponds(str)) {
+			return NONE;
+		}
+
+		return LITTLE;
+	}
 
 }
