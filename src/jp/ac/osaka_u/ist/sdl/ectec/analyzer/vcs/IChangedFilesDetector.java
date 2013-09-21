10a7184cebdd1481ef9d5ce719bc2403a768a7b4
k-hotta <k-hotta@ist.osaka-u.ac.jp>
2013-09-21 15:44:11 +0900

    create a class to detect changed files from a svn repository

--- src/jp/ac/osaka_u/ist/sdl/ectec/analyzer/vcs/IChangedFilesDetector.java
+++ src/jp/ac/osaka_u/ist/sdl/ectec/analyzer/vcs/IChangedFilesDetector.java
@@ -2,6 +2,8 @@ package jp.ac.osaka_u.ist.sdl.ectec.analyzer.vcs;
 
 import java.util.Map;
 
+import jp.ac.osaka_u.ist.sdl.ectec.settings.Language;
+
 /**
  * An interface for detecting added/deleted/changed files
  * 
@@ -14,10 +16,12 @@ public interface IChangedFilesDetector {
 	 * detects added/deleted/changed files
 	 * 
 	 * @param revisionIdentifier
+	 * @param language
 	 * @return
 	 * @throws Exception
 	 */
 	public Map<String, Character> detectChangedFiles(
-			final String revisionIdentifier) throws Exception;
+			final String revisionIdentifier, final Language language)
+			throws Exception;
 
 }
---
+++ src/jp/ac/osaka_u/ist/sdl/ectec/analyzer/vcs/svn/SVNChangedFilesDetector.java
@@ -0,0 +1,75 @@
+package jp.ac.osaka_u.ist.sdl.ectec.analyzer.vcs.svn;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import jp.ac.osaka_u.ist.sdl.ectec.analyzer.vcs.IChangedFilesDetector;
+import jp.ac.osaka_u.ist.sdl.ectec.settings.Language;
+
+import org.tmatesoft.svn.core.ISVNLogEntryHandler;
+import org.tmatesoft.svn.core.SVNException;
+import org.tmatesoft.svn.core.SVNLogEntry;
+import org.tmatesoft.svn.core.SVNLogEntryPath;
+import org.tmatesoft.svn.core.io.SVNRepository;
+
+/**
+ * A class that detects added/changed/deleted files from the given svn
+ * repository
+ * 
+ * @author k-hotta
+ * 
+ */
+public class SVNChangedFilesDetector implements IChangedFilesDetector {
+
+	/**
+	 * the manager of the target repository
+	 */
+	private final SVNRepositoryManager manager;
+
+	public SVNChangedFilesDetector(final SVNRepositoryManager manager) {
+		this.manager = manager;
+	}
+
+	@Override
+	public Map<String, Character> detectChangedFiles(
+			final String revisionIdentifier, final Language language)
+			throws Exception {
+		final long revision = Long.parseLong(revisionIdentifier);
+
+		final Map<String, Character> result = new HashMap<String, Character>();
+
+		final SVNRepository repository = manager.getRepository();
+
+		repository.log(null, revision, revision, true, false,
+				new ISVNLogEntryHandler() {
+					public void handleLogEntry(SVNLogEntry logEntry)
+							throws SVNException {
+
+						for (final Map.Entry<String, SVNLogEntryPath> entry : logEntry
+								.getChangedPaths().entrySet()) {
+
+							// in the case that source files are updated
+							if (language.isTarget(entry.getKey())) {
+								result.put(entry.getKey().substring(1), entry
+										.getValue().getType());
+								continue;
+							}
+
+							// in the case that directories are deleted
+							else if (('D' == entry.getValue().getType())
+									|| ('R' == entry.getValue().getType())) {
+								result.put(entry.getKey().substring(1), entry
+										.getValue().getType());
+								continue;
+							}
+						}
+					}
+				});
+
+		return Collections.unmodifiableMap(result);
+
+		return null;
+	}
+
+}
