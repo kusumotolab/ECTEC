c19a4b74b2382fa9ef5cd136dd03125ed1e7a648
k-hotta <k-hotta@ist.osaka-u.ac.jp>
2013-09-21 20:33:16 +0900

    create a class to create asts

---
+++ src/jp/ac/osaka_u/ist/sdl/ectec/analyzer/sourceanalyzer/ASTCreator.java
@@ -0,0 +1,24 @@
+package jp.ac.osaka_u.ist.sdl.ectec.analyzer.sourceanalyzer;
+
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.jdt.core.dom.AST;
+import org.eclipse.jdt.core.dom.ASTParser;
+import org.eclipse.jdt.core.dom.CompilationUnit;
+
+/**
+ * A class to create ASTs from the given source files
+ * 
+ * @author k-hotta
+ * 
+ */
+public class ASTCreator {
+
+	public static CompilationUnit createAST(final String sourceCode) {
+		final ASTParser parser = ASTParser.newParser(AST.JLS4);
+
+		parser.setSource(sourceCode.toCharArray());
+
+		return (CompilationUnit) parser.createAST(new NullProgressMonitor());
+	}
+
+}
