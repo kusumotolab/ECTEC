7cf09b037d2d88f8220b99cab62cb46568770e39
k-hotta <k-hotta@ist.osaka-u.ac.jp>
2013-09-21 18:55:45 +0900

    create an enum that represents types of changes on files

---
+++ src/jp/ac/osaka_u/ist/sdl/ectec/analyzer/filedetector/ChangeTypeOnFile.java
@@ -0,0 +1,13 @@
+package jp.ac.osaka_u.ist.sdl.ectec.analyzer.filedetector;
+
+/**
+ * An enum that represents types of changes on files
+ * 
+ * @author k-hotta
+ * 
+ */
+public enum ChangeTypeOnFile {
+
+	ADD, CHANGE, DELETE;
+
+}
