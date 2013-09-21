7831cdc3cc01d174b3c2ea665e90a6ad707017dd
k-hotta <k-hotta@ist.osaka-u.ac.jp>
2013-09-21 21:48:18 +0900

    create a class for registering crds

---
+++ src/jp/ac/osaka_u/ist/sdl/ectec/data/registerer/CRDRegisterer.java
@@ -0,0 +1,47 @@
+package jp.ac.osaka_u.ist.sdl.ectec.data.registerer;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import jp.ac.osaka_u.ist.sdl.ectec.data.CRD;
+import jp.ac.osaka_u.ist.sdl.ectec.db.DBConnectionManager;
+import jp.ac.osaka_u.ist.sdl.ectec.util.StringUtils;
+
+/**
+ * A class that represents a registerer for crd
+ * 
+ * @author k-hotta
+ * 
+ */
+public class CRDRegisterer extends AbstractElementRegisterer<CRD> {
+
+	/**
+	 * the constructor
+	 * 
+	 * @param dbManager
+	 * @param maxBatchCount
+	 */
+	public CRDRegisterer(DBConnectionManager dbManager, int maxBatchCount) {
+		super(dbManager, maxBatchCount);
+	}
+
+	@Override
+	protected String createPreparedStatementQueue() {
+		return "insert into CRD values(?,?,?,?,?,?,?)";
+	}
+
+	@Override
+	protected void setAttributes(PreparedStatement pstmt, CRD element)
+			throws SQLException {
+		int column = 0;
+		pstmt.setLong(++column, element.getId());
+		pstmt.setString(++column, element.getType().toString());
+		pstmt.setString(++column, element.getHead());
+		pstmt.setString(++column, element.getAnchor());
+		pstmt.setInt(++column, element.getCm());
+		pstmt.setString(++column,
+				StringUtils.convertListToString(element.getAncestors()));
+		pstmt.setString(++column, element.getFullText());
+	}
+
+}
