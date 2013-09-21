0d513846562f397efb6a727221940cb745c8cdd9
k-hotta <k-hotta@ist.osaka-u.ac.jp>
2013-09-21 21:29:11 +0900

    create an enum to represent types of blocks

---
+++ src/jp/ac/osaka_u/ist/sdl/ectec/data/BlockType.java
@@ -0,0 +1,13 @@
+package jp.ac.osaka_u.ist.sdl.ectec.data;
+
+/**
+ * An enum that represents types of blocks
+ * 
+ * @author k-hotta
+ * 
+ */
+public enum BlockType {
+
+	CLASS, METHOD, CATCH, DO, ELSE, ENHANCED_FOR, FINALLY, FOR, IF, SWITCH, SYNCHRONIZED, TRY, WHILE;
+
+}
