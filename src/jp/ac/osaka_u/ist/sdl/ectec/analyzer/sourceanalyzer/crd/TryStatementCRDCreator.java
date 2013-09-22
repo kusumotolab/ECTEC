4074f1cbe2eb246479dcca4f0034a9ac165d6878
k-hotta <k-hotta@ist.osaka-u.ac.jp>
2013-09-22 18:33:50 +0900

    implement a crd creator for try statements

---
+++ src/jp/ac/osaka_u/ist/sdl/ectec/analyzer/sourceanalyzer/crd/TryStatementCRDCreator.java
@@ -0,0 +1,53 @@
+package jp.ac.osaka_u.ist.sdl.ectec.analyzer.sourceanalyzer.crd;
+
+import java.util.List;
+
+import jp.ac.osaka_u.ist.sdl.ectec.data.BlockType;
+import jp.ac.osaka_u.ist.sdl.ectec.data.CRD;
+import jp.ac.osaka_u.ist.sdl.ectec.settings.Constants;
+
+import org.eclipse.jdt.core.dom.CatchClause;
+import org.eclipse.jdt.core.dom.TryStatement;
+
+/**
+ * A crd creator for try statements
+ * 
+ * @author k-hotta
+ * 
+ */
+public class TryStatementCRDCreator extends AbstractCRDCreator<TryStatement> {
+
+	public TryStatementCRDCreator(TryStatement node, List<CRD> ancestors) {
+		super(node, ancestors, BlockType.TRY);
+	}
+
+	/**
+	 * get the anchor (caught exceptions)
+	 */
+	@Override
+	protected String getAnchor() {
+		final StringBuilder builder = new StringBuilder();
+
+		@SuppressWarnings("rawtypes")
+		List catchClauses = node.catchClauses();
+
+		boolean catchAnyException = false;
+
+		for (Object obj : catchClauses) {
+			final CatchClause catchClause = (CatchClause) obj;
+			final String caughtExceptionType = catchClause.getException()
+					.getType().toString();
+			builder.append(caughtExceptionType + Constants.PREDICATE_DIVIDER);
+			catchAnyException = true;
+		}
+
+		if (catchAnyException) {
+			builder.delete(
+					builder.length() - Constants.PREDICATE_DIVIDER.length(),
+					builder.length());
+		}
+
+		return builder.toString();
+	}
+
+}
