f3dd2888ff3c337b9f6622ee92533c1eb497f108
k-hotta <k-hotta@ist.osaka-u.ac.jp>
2013-09-22 17:59:05 +0900

    create an interface to have constants

---
+++ src/jp/ac/osaka_u/ist/sdl/ectec/settings/Constants.java
@@ -0,0 +1,14 @@
+package jp.ac.osaka_u.ist.sdl.ectec.settings;
+
+/**
+ * An interface to have constants used in the processing
+ * 
+ * @author k-hotta
+ * 
+ */
+public interface Constants {
+
+	public static final String LINE_SEPARATOR = System
+			.getProperty("line.separator");
+
+}
