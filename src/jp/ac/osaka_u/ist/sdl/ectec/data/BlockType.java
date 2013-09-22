9210b5933d4a7349827c19e5883869d4d33645ab
k-hotta <k-hotta@ist.osaka-u.ac.jp>
2013-09-22 17:49:57 +0900

    add a field for the head strings

--- src/jp/ac/osaka_u/ist/sdl/ectec/data/BlockType.java
+++ src/jp/ac/osaka_u/ist/sdl/ectec/data/BlockType.java
@@ -8,6 +8,48 @@ package jp.ac.osaka_u.ist.sdl.ectec.data;
  */
 public enum BlockType {
 
-	CLASS, METHOD, CATCH, DO, ELSE, ENHANCED_FOR, FINALLY, FOR, IF, SWITCH, SYNCHRONIZED, TRY, WHILE;
+	CLASS("class"),
+
+	METHOD("method"),
+
+	CATCH("class"),
+
+	DO("do"),
+
+	ELSE("else"),
+
+	ENHANCED_FOR("for"),
+
+	FINALLY("finally"),
+
+	FOR("for"),
+
+	IF("if"),
+
+	SWITCH("switch"),
+
+	SYNCHRONIZED("synchronized"),
+
+	TRY("try"),
+
+	WHILE("while");
+
+	/**
+	 * the head string
+	 */
+	private final String head;
+
+	private BlockType(final String head) {
+		this.head = head;
+	}
+
+	/**
+	 * get the head string
+	 * 
+	 * @return
+	 */
+	public final String getHead() {
+		return head;
+	}
 
 }
