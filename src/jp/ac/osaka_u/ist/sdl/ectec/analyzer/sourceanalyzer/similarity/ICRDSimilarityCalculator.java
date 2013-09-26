747f0434b0b1978793d2970611e97b6fe92aeda7
kei-note <kei.hotta@gmail.com>
2013-09-26 22:33:30 +0900

    provide an interface for calculating similarities of crds

---
+++ src/jp/ac/osaka_u/ist/sdl/ectec/analyzer/sourceanalyzer/similarity/ICRDSimilarityCalculator.java
@@ -0,0 +1,23 @@
+package jp.ac.osaka_u.ist.sdl.ectec.analyzer.sourceanalyzer.similarity;
+
+import jp.ac.osaka_u.ist.sdl.ectec.data.CRD;
+
+/**
+ * An interface for calculating similarities between two crds
+ * 
+ * @author k-hotta
+ * 
+ */
+public interface ICRDSimilarityCalculator {
+
+	/**
+	 * calculate hash values between the given two crds <br>
+	 * this function must satisfy the symmetric law (calc(a, b) = calc(b, a))
+	 * 
+	 * @param crd
+	 * @param anotherCrd
+	 * @return
+	 */
+	public double calcSimilarity(final CRD crd, final CRD anotherCrd);
+
+}
