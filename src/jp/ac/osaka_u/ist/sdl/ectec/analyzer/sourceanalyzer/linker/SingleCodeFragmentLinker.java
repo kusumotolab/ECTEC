35597050f29ee6228dcd05396625d4741912dca7
kei-note <kei.hotta@gmail.com>
2013-09-28 14:20:22 +0900

    create skelton code for code fragment linkers

---
+++ src/jp/ac/osaka_u/ist/sdl/ectec/analyzer/sourceanalyzer/linker/SingleCodeFragmentLinker.java
@@ -0,0 +1,19 @@
+package jp.ac.osaka_u.ist.sdl.ectec.analyzer.sourceanalyzer.linker;
+
+import java.util.Collection;
+import java.util.Map;
+
+import jp.ac.osaka_u.ist.sdl.ectec.data.CodeFragmentInfo;
+import jp.ac.osaka_u.ist.sdl.ectec.data.CodeFragmentLinkInfo;
+
+public class SingleCodeFragmentLinker implements ICodeFragmentLinker {
+
+	@Override
+	public Map<Long, CodeFragmentLinkInfo> detectFragmentPairs(
+			Collection<CodeFragmentInfo> beforeBlocks,
+			Collection<CodeFragmentInfo> afterBlocks) {
+		// TODO implement
+		return null;
+	}
+
+}
