94cec2df7d59c40b9e816e38b8a250b41f7435c2
k-hotta <k-hotta@ist.osaka-u.ac.jp>
2013-09-29 20:47:34 +0900

    create a function to create new instances of code fragment genealogies

--- src/jp/ac/osaka_u/ist/sdl/ectec/data/CodeFragmentGenealogyInfo.java
+++ src/jp/ac/osaka_u/ist/sdl/ectec/data/CodeFragmentGenealogyInfo.java
@@ -1,6 +1,7 @@
 package jp.ac.osaka_u.ist.sdl.ectec.data;
 
 import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * A class that represents a genealogy of code fragments
@@ -11,10 +12,21 @@ import java.util.List;
 public class CodeFragmentGenealogyInfo extends
 		AbstractGenealogyInfo<CodeFragmentInfo, CodeFragmentLinkInfo> {
 
+	/**
+	 * the conter to have the number of created elements
+	 */
+	private static final AtomicLong count = new AtomicLong(0);
+
 	public CodeFragmentGenealogyInfo(final long id, final long startRevisionId,
 			final long endRevisionId, final List<Long> elements,
 			final List<Long> links) {
 		super(id, startRevisionId, endRevisionId, elements, links);
 	}
 
+	public CodeFragmentGenealogyInfo(final long startRevisionId,
+			final long endRevisionId, final List<Long> elements,
+			final List<Long> links) {
+		this(count.getAndIncrement(), startRevisionId, endRevisionId, elements,
+				links);
+	}
 }
