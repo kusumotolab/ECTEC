885118e779080e7e6033a65343302f6de804b23b
k-hotta <k-hotta@ist.osaka-u.ac.jp>
2013-09-30 00:07:31 +0900

    add some libraries

--- src/jp/ac/osaka_u/ist/sdl/ectec/information_retriever/fragment_genealogy/FragmentGenealogyPrinter.java
+++ src/jp/ac/osaka_u/ist/sdl/ectec/information_retriever/fragment_genealogy/FragmentGenealogyPrinter.java
@@ -5,7 +5,10 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.PrintWriter;
 import java.util.Map;
+import java.util.SortedMap;
+import java.util.TreeMap;
 
+import jp.ac.osaka_u.ist.sdl.ectec.analyzer.vcs.svn.SVNRepositoryManager;
 import jp.ac.osaka_u.ist.sdl.ectec.data.CRD;
 import jp.ac.osaka_u.ist.sdl.ectec.data.CodeFragmentGenealogyInfo;
 import jp.ac.osaka_u.ist.sdl.ectec.data.CodeFragmentInfo;
@@ -15,21 +18,32 @@ import jp.ac.osaka_u.ist.sdl.ectec.db.DBConnectionManager;
 
 public class FragmentGenealogyPrinter {
 
+	private static final String LINE_SEPARATOR = System
+			.getProperty("line.separator");
+
 	public static void main(final String[] args) {
 		DBConnectionManager dbManager = null;
 		PrintWriter pw = null;
+		SVNRepositoryManager repoManager = null;
 
 		try {
 			final String dbPath = args[0];
 			pw = new PrintWriter(new BufferedWriter(new FileWriter(new File(
 					args[1]))));
+			final String repositoryPath = args[2];
+			final String workingDir = args[3];
 
-			pw.println("B_Rev,B_File,B_Method,B_Start,B_End,A_Rev,A_File,A_Method,A_Start,A_End");
+			repoManager = new SVNRepositoryManager(repositoryPath, null, null,
+					null);
+
+			pw.println("ID,B_Rev,B_File,B_Method,B_Start,B_End,A_Rev,A_File,A_Method,A_Start,A_End");
 
 			dbManager = new DBConnectionManager(dbPath, 10000);
 
 			final Map<Long, CodeFragmentGenealogyInfo> fragmentGenealogies = dbManager
 					.getFragmentGenealogyRetriever().retrieveAll();
+
+			int index = 1;
 
 			for (final Map.Entry<Long, CodeFragmentGenealogyInfo> entry : fragmentGenealogies
 					.entrySet()) {
@@ -40,7 +54,8 @@ public class FragmentGenealogyPrinter {
 
 				final Map<Long, RevisionInfo> revisions = dbManager
 						.getRevisionRetriever().retrieveWithIds(
-								startRevisionId, endRevisionId);
+
+						startRevisionId, endRevisionId);
 				final Map<Long, CodeFragmentInfo> fragments = dbManager
 						.getFragmentRetriever().retrieveWithIds(
 								genealogy.getElements());
@@ -85,9 +100,44 @@ public class FragmentGenealogyPrinter {
 				final int aStart = endFragment.getStartLine();
 				final int aEnd = endFragment.getEndLine();
 
-				pw.println(bRev + "," + bFile + "," + bMethodName + ","
-						+ bStart + "," + bEnd + "," + aRev + "," + aFile + ","
-						+ aMethodName + "," + aStart + "," + aEnd);
+				pw.println(index + "," + bRev + "," + bFile + "," + bMethodName
+						+ "," + bStart + "," + bEnd + "," + aRev + "," + aFile
+						+ "," + aMethodName + "," + aStart + "," + aEnd);
+
+				try {
+					final SortedMap<Integer, String> beforeFileContents = getSrcLines(
+							repoManager, bRev, bFile);
+					final SortedMap<Integer, String> afterFileContents = getSrcLines(
+							repoManager, aRev, aFile);
+
+					final String bMethodContent = getMethodContent(
+							beforeFileContents, bStart, bEnd);
+					final String aMethodContent = getMethodContent(
+							afterFileContents, aStart, aEnd);
+
+					final String outputFile = workingDir + File.separator
+							+ index + ".txt";
+					final PrintWriter pw2 = new PrintWriter(new BufferedWriter(
+							new FileWriter(new File(outputFile))));
+					pw2.println("BEFORE_METHOD");
+					pw2.println("rev:" + bRev + ", " + bFile);
+					pw2.println("--");
+					pw2.println(bMethodContent);
+					pw2.println("--");
+					pw2.println();
+					pw2.println("AFTER_METHOD");
+					pw2.println("rev:" + aRev + ", " + aFile);
+					pw2.println("--");
+					pw2.println(aMethodContent);
+					pw2.println("--");
+					pw2.println("");
+
+					pw2.close();
+				} catch (Exception e) {
+					// ignore
+				}
+
+				index++;
 			}
 
 		} catch (Exception e) {
@@ -101,4 +151,30 @@ public class FragmentGenealogyPrinter {
 			}
 		}
 	}
+
+	private static SortedMap<Integer, String> getSrcLines(
+			final SVNRepositoryManager repoManager, final String rev,
+			final String file) throws Exception {
+		final String src = repoManager.getFileContents(Long.parseLong(rev),
+				file);
+		final SortedMap<Integer, String> srcLines = new TreeMap<Integer, String>();
+		int count = 1;
+		for (final String str : src.split(LINE_SEPARATOR)) {
+			srcLines.put(count++, str + LINE_SEPARATOR);
+		}
+		return srcLines;
+	}
+
+	private static String getMethodContent(
+			final SortedMap<Integer, String> fileContents, final int start,
+			final int end) {
+		final StringBuilder builder = new StringBuilder();
+
+		for (int i = start; i <= end; i++) {
+			builder.append(fileContents.get(i));
+		}
+
+		return builder.toString();
+	}
+
 }
