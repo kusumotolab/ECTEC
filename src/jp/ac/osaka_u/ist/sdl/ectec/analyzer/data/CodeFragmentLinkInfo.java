43296f1243bd05695a16d65a230a4556216996d6
k-hotta <k-hotta@ist.osaka-u.ac.jp>
2013-10-09 12:59:16 +0900

    create a data class for links of code fragments

---
+++ src/jp/ac/osaka_u/ist/sdl/ectec/analyzer/data/CodeFragmentLinkInfo.java
@@ -0,0 +1,109 @@
+package jp.ac.osaka_u.ist.sdl.ectec.analyzer.data;
+
+/**
+ * A class that represents links of code fragments
+ * 
+ * @author k-hotta
+ * 
+ */
+public class CodeFragmentLinkInfo extends AbstractElement implements
+		Comparable<CodeFragmentLinkInfo> {
+
+	/**
+	 * the before revision
+	 */
+	private final RevisionInfo beforeRevision;
+
+	/**
+	 * the after revision
+	 */
+	private final RevisionInfo afterRevision;
+
+	/**
+	 * the before fragment
+	 */
+	private final CodeFragmentInfo beforeFragment;
+
+	/**
+	 * the after fragment
+	 */
+	private final CodeFragmentInfo afterFragment;
+
+	public CodeFragmentLinkInfo(final long id,
+			final RevisionInfo beforeRevision,
+			final RevisionInfo afterRevision,
+			final CodeFragmentInfo beforeFragment,
+			final CodeFragmentInfo afterFragment) {
+		super(id);
+		this.beforeRevision = beforeRevision;
+		this.afterRevision = afterRevision;
+		this.beforeFragment = beforeFragment;
+		this.afterFragment = afterFragment;
+	}
+
+	/**
+	 * get the before revision
+	 * 
+	 * @return
+	 */
+	public final RevisionInfo getBeforeRevision() {
+		return beforeRevision;
+	}
+
+	/**
+	 * get the after revision
+	 * 
+	 * @return
+	 */
+	public final RevisionInfo getAfterRevision() {
+		return afterRevision;
+	}
+
+	/**
+	 * get the before fragment
+	 * 
+	 * @return
+	 */
+	public final CodeFragmentInfo getBeforeFragment() {
+		return beforeFragment;
+	}
+
+	/**
+	 * get the after fragment
+	 * 
+	 * @return
+	 */
+	public final CodeFragmentInfo getAfterFragment() {
+		return afterFragment;
+	}
+
+	@Override
+	public int compareTo(CodeFragmentLinkInfo another) {
+		final int compareWithBeforeRev = beforeRevision.compareTo(another
+				.getBeforeRevision());
+		if (compareWithBeforeRev != 0) {
+			return compareWithBeforeRev;
+		}
+
+		final int compareWithAfterRev = afterRevision.compareTo(another
+				.getAfterRevision());
+		if (compareWithAfterRev != 0) {
+			return compareWithAfterRev;
+		}
+
+		final int compareWithBeforeFragment = beforeFragment.compareTo(another
+				.getBeforeFragment());
+		if (compareWithBeforeFragment != 0) {
+			return compareWithBeforeFragment;
+		}
+
+		final int compareWithAfterFragment = afterFragment.compareTo(another
+				.getAfterFragment());
+		if (compareWithAfterFragment != 0) {
+			return compareWithAfterFragment;
+		}
+
+		return ((Long) id).compareTo(another.getId());
+	}
+
+}
