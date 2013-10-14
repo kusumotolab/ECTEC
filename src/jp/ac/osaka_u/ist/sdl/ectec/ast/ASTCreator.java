5b9b0e491b111281ab1412cdf2fb32e5865c36b7
k-hotta <k-hotta@ist.osaka-u.ac.jp>
2013-10-14 16:52:18 +0900

    implement a function to create an AST from a file

--- src/jp/ac/osaka_u/ist/sdl/ectec/ast/ASTCreator.java
+++ src/jp/ac/osaka_u/ist/sdl/ectec/ast/ASTCreator.java
@@ -1,5 +1,10 @@
 package jp.ac.osaka_u.ist.sdl.ectec.ast;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.jdt.core.dom.AST;
 import org.eclipse.jdt.core.dom.ASTParser;
@@ -19,6 +24,35 @@ public class ASTCreator {
 		parser.setSource(sourceCode.toCharArray());
 
 		return (CompilationUnit) parser.createAST(new NullProgressMonitor());
+	}
+
+	public static CompilationUnit creatAST(final File file) {
+		BufferedReader reader = null;
+		
+		try {
+			reader = new BufferedReader(new FileReader(file));
+			
+			final StringBuilder builder = new StringBuilder();
+			String line;
+			
+			while ((line = reader.readLine()) != null) {
+				builder.append(line + "\n");
+			}
+			
+			return createAST(builder.toString());
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		} finally {
+			if (reader != null) {
+				try {
+					reader.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
 	}
 
 }
