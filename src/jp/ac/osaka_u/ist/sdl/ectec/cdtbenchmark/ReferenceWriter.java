8295e00622c5777aa14f6c69806237846548e2ab
k-hotta <k-hotta@ist.osaka-u.ac.jp>
2013-10-15 10:55:30 +0200

    create a class to write a reference file for a given project from the reference file that has all the references among all the 8 projects

---
+++ src/jp/ac/osaka_u/ist/sdl/ectec/cdtbenchmark/ReferenceWriter.java
@@ -0,0 +1,45 @@
+package jp.ac.osaka_u.ist.sdl.ectec.cdtbenchmark;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+
+public class ReferenceWriter {
+
+	public static void main(String[] args) {
+		final String allRefFile = args[0];
+		final String outputFile = args[1];
+		final String project = args[2];
+
+		try {
+			final BufferedReader reader = new BufferedReader(new FileReader(
+					new File(allRefFile)));
+			final PrintWriter pw = new PrintWriter(new BufferedWriter(
+					new FileWriter(new File(outputFile))));
+
+			String line;
+
+			while ((line = reader.readLine()) != null) {
+				final String[] split = line.split("\t");
+				if (split[2].startsWith(project + "/")) {
+					for (int i = 0; i < split.length - 1; i++) {
+						if (i != 1) {
+							pw.print(split[i] + "\t");
+						}
+					}
+					pw.println(split[split.length - 1]);
+				}
+			}
+
+			reader.close();
+			pw.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
