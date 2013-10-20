7b481249d953d1fed950e765edf959190e8ea3bb
k-hotta <k-hotta@ist.osaka-u.ac.jp>
2013-10-20 22:44:02 +0900

    [bug fix] wrong type argument

--- src/jp/ac/osaka_u/ist/sdl/ectec/analyzer/data/MethodInfo.java
+++ src/jp/ac/osaka_u/ist/sdl/ectec/analyzer/data/MethodInfo.java
@@ -2,7 +2,7 @@ package jp.ac.osaka_u.ist.sdl.ectec.analyzer.data;
 
 import jp.ac.osaka_u.ist.sdl.ectec.db.data.BlockType;
 
-import org.eclipse.jdt.core.dom.TypeDeclaration;
+import org.eclipse.jdt.core.dom.MethodDeclaration;
 
 /**
  * A class that represents methods
@@ -10,11 +10,11 @@ import org.eclipse.jdt.core.dom.TypeDeclaration;
  * @author k-hotta
  * 
  */
-public class MethodInfo extends BlockInfo<TypeDeclaration> {
+public class MethodInfo extends BlockInfo<MethodDeclaration> {
 
 	public MethodInfo(long id, FileInfo ownerFile, CRD crd,
 			RevisionInfo startRevision, RevisionInfo endRevision,
-			int startLine, int endLine, int size, TypeDeclaration node) {
+			int startLine, int endLine, int size, MethodDeclaration node) {
 		super(id, ownerFile, crd, startRevision, endRevision, startLine,
 				endLine, size, BlockType.METHOD, node);
 	}
