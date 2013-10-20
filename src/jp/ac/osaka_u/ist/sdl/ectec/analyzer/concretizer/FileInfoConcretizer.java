e0d3b16bc17a2d2711cb61ce4aab373d488398c3
k-hotta <k-hotta@ist.osaka-u.ac.jp>
2013-10-20 21:32:58 +0900

    create a class to concretize files

--- src/jp/ac/osaka_u/ist/sdl/ectec/analyzer/concretizer/FileInfoConcretizer.java
+++ src/jp/ac/osaka_u/ist/sdl/ectec/analyzer/concretizer/FileInfoConcretizer.java
@@ -1,5 +1,18 @@
 package jp.ac.osaka_u.ist.sdl.ectec.analyzer.concretizer;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
+import java.util.TreeMap;
+
+import jp.ac.osaka_u.ist.sdl.ectec.analyzer.data.FileInfo;
+import jp.ac.osaka_u.ist.sdl.ectec.analyzer.data.RevisionInfo;
+import jp.ac.osaka_u.ist.sdl.ectec.ast.ASTCreator;
+import jp.ac.osaka_u.ist.sdl.ectec.db.data.DBFileInfo;
+import jp.ac.osaka_u.ist.sdl.ectec.detector.vcs.IRepositoryManager;
+
+import org.eclipse.jdt.core.dom.CompilationUnit;
+
 /**
  * A class for concretizing files
  * 
@@ -7,5 +20,62 @@ package jp.ac.osaka_u.ist.sdl.ectec.analyzer.concretizer;
  * 
  */
 public final class FileInfoConcretizer {
+
+	/**
+	 * the repository manager
+	 */
+	private final IRepositoryManager repositoryManager;
+
+	public FileInfoConcretizer(final IRepositoryManager repositoryManager) {
+		this.repositoryManager = repositoryManager;
+	}
+
+	public FileInfo concretize(final DBFileInfo dbFile,
+			final Map<Long, RevisionInfo> revisions) {
+		try {
+			final long id = dbFile.getId();
+			final RevisionInfo startRevision = revisions.get(dbFile
+					.getStartRevisionId());
+			final RevisionInfo endRevision = revisions.get(dbFile
+					.getEndRevisionId());
+
+			if (startRevision == null || endRevision == null) {
+				return null;
+			}
+
+			final String src = repositoryManager.getFileContents(
+					startRevision.getIdentifier(), dbFile.getPath());
+
+			final CompilationUnit root = ASTCreator.createAST(src);
+
+			return new FileInfo(id, dbFile.getPath(), startRevision,
+					endRevision, root);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	public final Map<Long, FileInfo> concretizeAll(
+			final Collection<DBFileInfo> dbFiles,
+			final Map<Long, RevisionInfo> revisions) {
+		final Map<Long, FileInfo> result = new TreeMap<Long, FileInfo>();
+
+		for (final DBFileInfo dbFile : dbFiles) {
+			final FileInfo file = concretize(dbFile, revisions);
+			if (file != null) {
+				result.put(file.getId(), file);
+			}
+		}
+
+		return Collections.unmodifiableMap(result);
+	}
+
+	public final Map<Long, FileInfo> concretizeAll(
+			final Map<Long, DBFileInfo> dbFiles,
+			final Map<Long, RevisionInfo> revisions) {
+		return concretizeAll(dbFiles.values(), revisions);
+	}
 
 }
