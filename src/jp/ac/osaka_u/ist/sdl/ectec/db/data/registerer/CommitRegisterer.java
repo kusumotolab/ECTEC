472e4027339678c0769bb806dfd388c0dc330811
k-hotta <k-hotta@ist.osaka-u.ac.jp>
2014-04-03 15:00:03 +0900

    change CommitRegisterer to fit the new scheme

--- src/jp/ac/osaka_u/ist/sdl/ectec/db/data/registerer/CommitRegisterer.java
+++ src/jp/ac/osaka_u/ist/sdl/ectec/db/data/registerer/CommitRegisterer.java
@@ -2,6 +2,8 @@ package jp.ac.osaka_u.ist.sdl.ectec.db.data.registerer;
 
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.Calendar;
+import java.util.Date;
 
 import jp.ac.osaka_u.ist.sdl.ectec.db.DBConnectionManager;
 import jp.ac.osaka_u.ist.sdl.ectec.db.data.DBCommitInfo;
@@ -20,7 +22,7 @@ public class CommitRegisterer extends AbstractElementRegisterer<DBCommitInfo> {
 
 	@Override
 	protected String createPreparedStatementQueue() {
-		return "insert into VCS_COMMIT values (?,?,?,?,?)";
+		return "insert into VCS_COMMIT values (?,?,?,?,?,?,?,?,?)";
 	}
 
 	@Override
@@ -30,8 +32,18 @@ public class CommitRegisterer extends AbstractElementRegisterer<DBCommitInfo> {
 		pstmt.setLong(++column, element.getId());
 		pstmt.setLong(++column, element.getBeforeRevisionId());
 		pstmt.setLong(++column, element.getAfterRevisionId());
-		pstmt.setString(++column, element.getBeforeRevisionIdentifier());
-		pstmt.setString(++column, element.getAfterRevisionIdentifier());
+
+		final Date date = element.getDate();
+		final Calendar calendar = Calendar.getInstance();
+		calendar.setTime(date);
+
+		pstmt.setInt(++column, calendar.get(Calendar.YEAR));
+		// "+1" is necessary to convert 0-11 to 1-12
+		pstmt.setInt(++column, calendar.get(Calendar.MONTH) + 1);
+		pstmt.setInt(++column, calendar.get(Calendar.DAY_OF_MONTH));
+		pstmt.setInt(++column, calendar.get(Calendar.HOUR));
+		pstmt.setInt(++column, calendar.get(Calendar.MINUTE));
+		pstmt.setInt(++column, calendar.get(Calendar.SECOND));
 	}
 
 }
