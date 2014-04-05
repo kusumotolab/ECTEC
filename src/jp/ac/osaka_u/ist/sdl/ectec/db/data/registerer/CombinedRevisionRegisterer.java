97b5e98ed666bfc4bf1c80afdba7e6de935438c2
k-hotta <k-hotta@ist.osaka-u.ac.jp>
2014-04-05 16:30:38 +0900

    create a class to register combined revisions

--- src/jp/ac/osaka_u/ist/sdl/ectec/db/data/registerer/CombinedRevisionRegisterer.java
+++ src/jp/ac/osaka_u/ist/sdl/ectec/db/data/registerer/CombinedRevisionRegisterer.java
@@ -1,5 +1,9 @@
 package jp.ac.osaka_u.ist.sdl.ectec.db.data.registerer;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Collection;
+
 import jp.ac.osaka_u.ist.sdl.ectec.db.DBConnectionManager;
 import jp.ac.osaka_u.ist.sdl.ectec.db.data.DBCombinedRevisionInfo;
 
@@ -10,11 +14,33 @@ import jp.ac.osaka_u.ist.sdl.ectec.db.data.DBCombinedRevisionInfo;
  * 
  */
 public class CombinedRevisionRegisterer extends
-		AbstractElementRegisterer<DBCombinedRevisionInfo> {
+		AbstractNonuniqueElementRegisterer<DBCombinedRevisionInfo> {
 
 	public CombinedRevisionRegisterer(DBConnectionManager dbManager,
 			int maxBatchCount) {
 		super(dbManager, maxBatchCount);
+	}
+
+	@Override
+	protected String createPreparedStatementQueue() {
+		return "insert into COMBINED_REVISION";
+	}
+
+	@Override
+	protected int makePreparedStatements(PreparedStatement pstmt,
+			DBCombinedRevisionInfo element) throws SQLException {
+		final long elementId = element.getId();
+		final Collection<Long> originalRevisions = element.getOriginalRevisions();
+		
+		for (final long originalRevisionId : originalRevisions) {
+			int column = 0;
+			pstmt.setLong(++column, elementId);
+			pstmt.setLong(++column, originalRevisionId);
+			
+			pstmt.addBatch();
+		}
+		
+		return element.getOriginalRevisions().size();
 	}
 
 }
