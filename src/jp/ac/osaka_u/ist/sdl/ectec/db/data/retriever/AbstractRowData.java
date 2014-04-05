dac875b7b9b49a0eff6e7b1eed3288d953040e41
k-hotta <k-hotta@ist.osaka-u.ac.jp>
2014-04-05 22:02:11 +0900

    create a class to represent a row in db tables

---
+++ src/jp/ac/osaka_u/ist/sdl/ectec/db/data/retriever/AbstractRowData.java
@@ -0,0 +1,11 @@
+package jp.ac.osaka_u.ist.sdl.ectec.db.data.retriever;
+
+/**
+ * A class that represents a row in a db table
+ * 
+ * @author k-hotta
+ * 
+ */
+public abstract class AbstractRowData {
+
+}
