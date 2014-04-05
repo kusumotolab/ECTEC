d8f800b1232e05c5587ee754322def2c9ce5500c
k-hotta <k-hotta@ist.osaka-u.ac.jp>
2014-04-05 18:05:27 +0900

    change AbstractUniqueElementRetriever to inheritate AbstractElementRetriever

--- src/jp/ac/osaka_u/ist/sdl/ectec/db/data/retriever/AbstractUniqueElementRetriever.java
+++ src/jp/ac/osaka_u/ist/sdl/ectec/db/data/retriever/AbstractUniqueElementRetriever.java
@@ -2,11 +2,7 @@ package jp.ac.osaka_u.ist.sdl.ectec.db.data.retriever;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.Collection;
 import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
 import java.util.SortedMap;
 import java.util.TreeMap;
 
@@ -14,143 +10,30 @@ import jp.ac.osaka_u.ist.sdl.ectec.db.DBConnectionManager;
 import jp.ac.osaka_u.ist.sdl.ectec.db.data.AbstractDBElement;
 
 /**
- * An abstract class to retrieve elements from db
+ * An abstract class to retrieve unique elements from db
  * 
  * @author k-hotta
  * 
  * @param <T>
  */
-public abstract class AbstractUniqueElementRetriever<T extends AbstractDBElement> {
-
-	/**
-	 * the manager of the connection between db
-	 */
-	protected final DBConnectionManager dbManager;
+public abstract class AbstractUniqueElementRetriever<T extends AbstractDBElement>
+		extends AbstractElementRetriever<T> {
 
 	public AbstractUniqueElementRetriever(final DBConnectionManager dbManager) {
-		this.dbManager = dbManager;
+		super(dbManager);
 	}
 
-	/**
-	 * retrieve elements with the given query
-	 * 
-	 * @param query
-	 * @return
-	 * @throws SQLException
-	 */
-	public synchronized SortedMap<Long, T> retrieve(final String query)
+	@Override
+	public SortedMap<Long, T> instantiate(final ResultSet rs)
 			throws SQLException {
 		final SortedMap<Long, T> result = new TreeMap<Long, T>();
-
-		final Statement stmt = dbManager.createStatement();
-		final ResultSet rs = stmt.executeQuery(query);
 
 		while (rs.next()) {
 			final T element = createElement(rs);
 			result.put(element.getId(), element);
 		}
 
-		stmt.close();
-		rs.close();
-
 		return Collections.unmodifiableSortedMap(result);
-	}
-
-	/**
-	 * retrieve all elements stored into the db
-	 * 
-	 * @return
-	 * @throws SQLException
-	 */
-	public synchronized SortedMap<Long, T> retrieveAll() throws SQLException {
-		final String query = "select * from " + getTableName();
-		return retrieve(query);
-	}
-
-	/**
-	 * retrieve elements having one of the given id
-	 * 
-	 * @param ids
-	 * @return
-	 * @throws SQLException
-	 */
-	public synchronized SortedMap<Long, T> retrieveWithIds(
-			final Collection<Long> ids) throws SQLException {
-		if (ids.isEmpty()) {
-			return new TreeMap<Long, T>();
-		}
-		final StringBuilder builder = new StringBuilder();
-		builder.append("select * from " + getTableName() + " where "
-				+ getIdColumnName() + " in (");
-
-		for (final long id : ids) {
-			builder.append(id + ",");
-		}
-
-		builder.deleteCharAt(builder.length() - 1);
-		builder.append(")");
-
-		return retrieve(builder.toString());
-	}
-
-	/**
-	 * retrieve elements having one of the given id
-	 * 
-	 * @param ids
-	 * @return
-	 * @throws SQLException
-	 */
-	public synchronized SortedMap<Long, T> retrieveWithIds(long... ids)
-			throws SQLException {
-		final Set<Long> idSet = new HashSet<Long>();
-		for (final long id : ids) {
-			idSet.add(id);
-		}
-
-		return retrieveWithIds(idSet);
-	}
-
-	/**
-	 * retrieve elements NOT having one of the given id
-	 * 
-	 * @param ids
-	 * @return
-	 * @throws SQLException
-	 */
-	public synchronized SortedMap<Long, T> retrieveWithoutIds(
-			final Collection<Long> ids) throws SQLException {
-		if (ids.isEmpty()) {
-			return new TreeMap<Long, T>();
-		}
-		final StringBuilder builder = new StringBuilder();
-		builder.append("select * from " + getTableName() + " where "
-				+ getIdColumnName() + " not in (");
-
-		for (final long id : ids) {
-			builder.append(id + ",");
-		}
-
-		builder.deleteCharAt(builder.length() - 1);
-		builder.append(")");
-
-		return retrieve(builder.toString());
-	}
-
-	/**
-	 * retrieve elements NOT having one of the given id
-	 * 
-	 * @param ids
-	 * @return
-	 * @throws SQLException
-	 */
-	public synchronized SortedMap<Long, T> retrieveWithoutIds(long... ids)
-			throws SQLException {
-		final Set<Long> idSet = new HashSet<Long>();
-		for (final long id : ids) {
-			idSet.add(id);
-		}
-
-		return retrieveWithoutIds(idSet);
 	}
 
 	/**
@@ -161,19 +44,5 @@ public abstract class AbstractUniqueElementRetriever<T extends AbstractDBElement
 	 * @throws SQLException
 	 */
 	protected abstract T createElement(ResultSet rs) throws SQLException;
-
-	/**
-	 * get the name of the table
-	 * 
-	 * @return
-	 */
-	protected abstract String getTableName();
-
-	/**
-	 * get the name of the column having ids
-	 * 
-	 * @return
-	 */
-	protected abstract String getIdColumnName();
 
 }
