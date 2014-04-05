85735da0ee910d0fef40e40b97ea1fb31d95c1d8
k-hotta <k-hotta@ist.osaka-u.ac.jp>
2014-04-05 18:18:21 +0900

    create a class to retrieve repositories

---
+++ src/jp/ac/osaka_u/ist/sdl/ectec/db/data/retriever/RepositoryRetriever.java
@@ -0,0 +1,41 @@
+package jp.ac.osaka_u.ist.sdl.ectec.db.data.retriever;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import jp.ac.osaka_u.ist.sdl.ectec.db.DBConnectionManager;
+import jp.ac.osaka_u.ist.sdl.ectec.db.data.DBRepositoryInfo;
+
+/**
+ * A class for retrieving repositories
+ * 
+ * @author k-hotta
+ * 
+ */
+public class RepositoryRetriever extends
+		AbstractUniqueElementRetriever<DBRepositoryInfo> {
+
+	public RepositoryRetriever(final DBConnectionManager dbManager) {
+		super(dbManager);
+	}
+
+	@Override
+	protected DBRepositoryInfo createElement(ResultSet rs) throws SQLException {
+		int column = 0;
+		final long id = rs.getLong(++column);
+		final String url = rs.getString(++column);
+		
+		return new DBRepositoryInfo(id, url);
+	}
+
+	@Override
+	protected String getTableName() {
+		return "REPOSITORY";
+	}
+
+	@Override
+	protected String getIdColumnName() {
+		return "REPOSITORY_ID";
+	}
+
+}
