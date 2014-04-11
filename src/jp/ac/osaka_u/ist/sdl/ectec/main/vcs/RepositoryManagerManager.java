41f600f6b65acfe95881bd92d1c0af9872bc7fea
k-hotta <k-hotta@ist.osaka-u.ac.jp>
2014-04-11 15:07:57 +0900

    make RepositoryManagerManager to have multiple repository managers

--- src/jp/ac/osaka_u/ist/sdl/ectec/main/vcs/RepositoryManagerManager.java
+++ src/jp/ac/osaka_u/ist/sdl/ectec/main/vcs/RepositoryManagerManager.java
@@ -1,7 +1,14 @@
 package jp.ac.osaka_u.ist.sdl.ectec.main.vcs;
 
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+import jp.ac.osaka_u.ist.sdl.ectec.LoggingManager;
+import jp.ac.osaka_u.ist.sdl.ectec.db.data.DBRepositoryInfo;
 import jp.ac.osaka_u.ist.sdl.ectec.main.vcs.svn.SVNRepositoryManager;
 import jp.ac.osaka_u.ist.sdl.ectec.settings.VersionControlSystem;
+
+import org.apache.log4j.Logger;
 
 /**
  * A class that manages the repository manager
@@ -12,43 +19,76 @@ import jp.ac.osaka_u.ist.sdl.ectec.settings.VersionControlSystem;
 public class RepositoryManagerManager {
 
 	/**
-	 * the repository manager
+	 * the logger
 	 */
-	private final IRepositoryManager repositoryManager;
+	private static final Logger logger = LoggingManager
+			.getLogger(RepositoryManagerManager.class.getName());
 
 	/**
-	 * the constructor
+	 * the repository manager
+	 */
+	private final ConcurrentMap<Long, IRepositoryManager> repositoryManagers;
+
+	public RepositoryManagerManager() {
+		this.repositoryManagers = new ConcurrentHashMap<Long, IRepositoryManager>();
+	}
+
+	/**
+	 * add a repository manager
 	 * 
+	 * @param repository
 	 * @param vcs
+	 * @throws Exception
+	 */
+	public void addRepositoryManager(final DBRepositoryInfo repository,
+			final VersionControlSystem vcs) throws Exception {
+		addRepositoryManager(repository.getId(), repository.getUrl(), null,
+				null, null, vcs);
+	}
+
+	/**
+	 * add a repository manager
+	 * 
+	 * @param id
 	 * @param url
 	 * @param userName
 	 * @param passwd
 	 * @param additionalUrl
+	 * @param vcs
 	 * @throws Exception
-	 *             thrown if it is failed to initialize the repository
 	 */
-	public RepositoryManagerManager(final VersionControlSystem vcs,
-			final String url, final String userName, final String passwd,
-			final String additionalUrl, final int threadsCount)
+	public void addRepositoryManager(final long id, final String url,
+			final String userName, final String passwd,
+			final String additionalUrl, final VersionControlSystem vcs)
 			throws Exception {
+		IRepositoryManager repositoryManager = null;
+
 		switch (vcs) {
 		case SVN:
 			repositoryManager = new SVNRepositoryManager(url, userName, passwd,
 					additionalUrl);
 			break;
 		default:
-			throw new RepositoryNotInitializedException(
-					"the version control system was not specified");
+			break;
 		}
+
+		if (repositoryManager == null) {
+			throw new IllegalStateException(
+					"cannot create a repository manager for repository " + id);
+		}
+
+		repositoryManagers.put(id, repositoryManager);
+		logger.info("initialize the repository manager for repository " + id);
 	}
 
 	/**
 	 * get the repository manager
 	 * 
+	 * @param id
 	 * @return
 	 */
-	public final IRepositoryManager getRepositoryManager() {
-		return this.repositoryManager;
+	public IRepositoryManager getRepositoryManager(final long id) {
+		return repositoryManagers.get(id);
 	}
 
 }
