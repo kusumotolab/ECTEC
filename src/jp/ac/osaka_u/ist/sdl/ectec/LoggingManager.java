c34547adf5846e10f4a9b9854bff9559f393c072
k-hotta <k-hotta@ist.osaka-u.ac.jp>
2016-01-03 04:03:34 +0900

    check if the default logging file exists

--- src/jp/ac/osaka_u/ist/sdl/ectec/LoggingManager.java
+++ src/jp/ac/osaka_u/ist/sdl/ectec/LoggingManager.java
@@ -1,5 +1,7 @@
 package jp.ac.osaka_u.ist.sdl.ectec;
 
+import java.io.File;
+
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
@@ -18,9 +20,12 @@ public class LoggingManager {
 	private static final String DEFAULT_LOGGING_PROPERTIES = "log4j.properties";
 
 	static {
-		PropertyConfigurator.configure(DEFAULT_LOGGING_PROPERTIES);
+		final File file = new File(DEFAULT_LOGGING_PROPERTIES);
+		if (file.exists()) {
+			PropertyConfigurator.configure(DEFAULT_LOGGING_PROPERTIES);
+		}
 	}
-	
+
 	public static Logger getLogger(final String name) {
 		return LogManager.getLogger(name);
 	}
